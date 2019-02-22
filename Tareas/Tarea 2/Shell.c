dae22824a2e748dad09cf21aae8af198ef83c7d7
cderian <c.derian_11@ciencias.unam.mx>
2019-02-22 12:30:40 -0600

    Comando ls a medias

--- Tareas/Tarea 2/Shell.c
+++ Tareas/Tarea 2/Shell.c
@@ -5,11 +5,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define COMANDO_VALIDO(comando, entrada) (strcmp (comando, entrada) == 0)
 
-void procesarComando()
+int ls(const char * arg)
 {
+	int status;
+	pid_t pid;
+	pid = fork();
 
+	if(pid==0)
+	{
+		execl("/bin/ls", "/bin/ls", arg, NULL);
+		exit(EXIT_FAILURE);
+	}
+	else if(pid < 0)
+	{
+		status = -1;
+	}
+	else
+	{
+		status = -1;
+	}
+
+	return status;
 }
 
 int main(int argc, char const *argv[])
@@ -26,7 +47,7 @@ int main(int argc, char const *argv[])
 		}
 		else if (COMANDO_VALIDO(comando, "ls"))
 		{
-			printf("Comando ls aún no implementado\n");
+			ls(argv[1]);
 		}
 		else if (COMANDO_VALIDO(comando, "pwd"))
 		{
